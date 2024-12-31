3296e1bbeb3abcc141a08945237baa6d3e29aebb
444B <15311579+444B@users.noreply.github.com.>
2024-12-31 17:26:33 +0000

    testing mypy

--- src/app.py
+++ src/app.py
@@ -1,8 +1,22 @@
-import streamlit as st
+# import streamlit as st
 
-def main():
-    st.title("st-template")
+# def main():
+#     st.title("st-template")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+def show_count(count, word):
+    if count == 1:
+        return f"1 {word}"
+    count_str = str(count) if count else "no"
+    return f"{count_str} {word}s"
+
+
+show_count(99, "bird")
+
+show_count(1, "bird")
+
+show_count(0, "bird")
